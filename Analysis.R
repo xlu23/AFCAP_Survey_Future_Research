e71116abab6e7e4fe02231fbd8c440be12f559d7
Joe Walsh <j.thomas.walsh@gmail.com>
2014-11-18 14:56:23 +0000

    Fixed the data calls.  The analysis requires the data in two formats.  The script called the formats in the wrong order, leading to errors when I tried to do the analysis.  I fixed the order.
    I also deleted an unnecessary bootstrap function from the script.

--- Analysis.R
+++ Analysis.R
@@ -26,8 +26,7 @@ library(dendextend)
 
 
 ## LOAD DATA
-data <- #read.csv("/home/ubuntu/modified_data.csv") 
-  read.csv("/home/ubuntu/modified_data using joint ranks.csv", header = TRUE) #, na.strings = "missing")
+data <- read.csv("/home/ubuntu/modified_data using joint ranks.csv", header = TRUE) #, na.strings = "missing")
 
   # Drop 'interested in answering one last question'
   data <- data[, -grep("Interested", names(data))]
@@ -36,20 +35,53 @@ data <- #read.csv("/home/ubuntu/modified_data.csv")
   # Drop PCS questions for this paper
   data <- data[, -grep("PCS", names(data))]
 
-table(data$access.from.continent, data$research.school.pedestrian)
-table(data$access.goods2market)
+
+
+  # # Survey continent and country
+  # URL <- paste('http://www.iplocation.net/index.php?query=',
+  #              data$IP.Address,
+  #              sep='')
+  # webpages <- getURL(url = URL)
+  # 
+  # data$continent <- sapply(X=webpages, FUN=function(x) as.character(readHTMLTable(x)[[13]][7,2]))
+  # data$country <- sapply(X=webpages, FUN=function(x) as.character(readHTMLTable(x)[[12]][3,2]))
+  # 
+  # round(x=table(data$continent) / nrow(data), 
+  #       digits=2)
+  # 
+  # round(x=table(data$country) / nrow(data), 
+  #       digits=2)
+
+
+  # Survey collection dates
+  summary( as.Date(data$StartDate) )
+    
+  # Time to take the survey (minutes)
+  summary( as.numeric(data$survey_length_of_time) )
+    
+  # Good participation (% that agreed to answer a few questions)
+  100 * round( mean( as.character(data$Interested_in_answering_one_last_question_) == 'Yes', na.rm = TRUE), 2)
+
+  # Bivariate relationships
+  table(data$access.from.continent, data$research.school.pedestrian)
+  table(data$access.goods2market)
 
   table(data$specialty, data$need.research.DUI)
   table(data$need.research.aggressive.driving, data$inadequate.attention.dust)
   table(data$need.research.DUI, data$specialty)
 
   # Hierarchical clustering
-  hclust <- hclustvar(X.quali = data[, -c(23)])
-  stability(hclust, B=50)
-  u <- cutree(tree=hclust, k=8)
-  sort(u)
-  cutreevar(hclust, k=8)
-  plot(hclust, main="Cluster Analysis Dendrogram")
+  # don't include country where the survey was taken
+  # the algorithm crashes when we include it
+  hclust <- hclustvar(X.quali = data[, -c(21)])
+  stability(hclust, B=25)
+  cutree(hclust, k=3)
+
+  cutreevar(hclust, k=3)
+  pdf("dendrogram.pdf")
+    plot(hclust, main="Cluster Analysis Dendrogram")
+  dev.off()
+
 
 
 rules <- apriori(data, parameter = list(supp = 0.025, conf = 1))
@@ -68,19 +100,34 @@ options(digits=2)
 
 ## DESCRIPTIVE STATISTICS
 
+  ## Data in different format (e.g. most, neutral, least are separate variables)
+  data <- read.csv("/home/ubuntu/modified_data.csv") 
+
+  # Drop 'interested in answering one last question'
+  data <- data[, -grep("Interested", names(data))]
+  data <- data[, -grep("length", names(data))]
+  
+  # Drop PCS questions for this paper
+  data <- data[, -grep("PCS", names(data))]
+
+
   # Affiliation (%)
   temp <- 100 * round( sort(table(data$affiliation), decreasing = TRUE) / sum(!is.na(data$affiliation)), 2)
     names(temp)[ grep("Public safety", names(temp)) ] <- "Public safety/\nlaw enforcement"
     names(temp)[ grep("Other", names(temp)) ] <- "Other"
-  par(mar=c(10,4,1,0))
-  barplot(height=temp, ylab="% of respondents", las=2)
+  pdf("affiliation.pdf")
+    par(mar=c(10,4,1,0))
+    barplot(height=temp, ylab="% of respondents", las=2)
+  dev.off()
 
   # Professional speciality
   temp <- 100 * round( sort(table(data$specialty), decreasing = TRUE) / sum(!is.na(data$specialty)), 2)
     names(temp)[ grep("Environmental", names(temp)) ] <- "Environmental\nprotection"
     names(temp)[ grep("Other", names(temp)) ] <- "Other"
-  par(mar=c(8,4,1,0))
-  barplot(temp, ylab="% of respondents", las=2)
+  pdf("specialty.pdf")
+    par(mar=c(8,4,1,0))
+    barplot(temp, ylab="% of respondents", las=2)
+  dev.off()
 
 
   # Access from country (%)
@@ -98,11 +145,13 @@ options(digits=2)
               dangerous = table(data$dangerous.mode),
               least_dangerous = table(data$least.dangerous.mode) )
   temp <- temp[c(2,3,1),]
-  par(mar=c(1,1,1,1))
-  mosaicplot(temp, las=1, col=c("grey50", "grey70", "grey90"), main="")
-  text(x=c(.28, .57, .85), y=c(.85, .8, .775), labels=c("16", "22", "22"))
-  text(x=c(.28, .57, .85), y=c(.55, .5, .35), labels=c("16", "15", "29"))
-  text(x=c(.28, .57, .85), y=c(.20, .175, .03), labels=c("23", "24", "7"))
+  pdf("dangerous_modes.pdf")
+    par(mar=c(1,1,1,1))
+    mosaicplot(temp, las=1, col=c("grey50", "grey70", "grey90"), main="")
+    text(x=c(.28, .57, .85), y=c(.85, .8, .775), labels=c("16", "22", "22"))
+    text(x=c(.28, .57, .85), y=c(.55, .5, .35), labels=c("16", "15", "29"))
+    text(x=c(.28, .57, .85), y=c(.20, .175, .03), labels=c("23", "24", "7"))
+  dev.off()
 
   # Weighted average
   apply(temp, 1, function(x) (5*x[1] + 3*x[2] + x[3]) / sum(x) )
@@ -118,13 +167,16 @@ options(digits=2)
     rownames(temp) <- c("very important", "important", "moderately important", "unimportant", "very unimportant")
     colnames(temp) <- c("Travelway", "Signage", "Dust")
     temp <- temp[,c(3,1,2)]
-    mosaicplot(t(temp), main="", las=1, 
-               col=paste0("grey", 5:10*10)) 
-    text(x=c(.335, .6, .865), y=c(.85, .8, .8), labels=c("16", "22", "22"))
-    text(x=c(.335, .6, .865), y=c(.625, .475, .475), labels=c("11", "17", "19"))
-    text(x=c(.335, .6, .865), y=c(.35, .225, .215), labels=c("22", "14", "10"))
-    text(x=c(.335, .6, .865), y=c(.09, .09, .04), labels=c("5", "6", "5"))
-    text(x=c(.335, .6), y=.00, labels=c("4", "4"))
+    pdf("road_conditions_rural_crashes.pdf")
+      mosaicplot(t(temp), main="", las=1, 
+                 col=paste0("grey", 5:10*10)) 
+      text(x=c(.335, .6, .865), y=c(.85, .8, .8), labels=c("16", "22", "22"))
+      text(x=c(.335, .6, .865), y=c(.625, .475, .475), labels=c("11", "17", "19"))
+      text(x=c(.335, .6, .865), y=c(.35, .225, .215), labels=c("22", "14", "10"))
+      text(x=c(.335, .6, .865), y=c(.09, .09, .04), labels=c("5", "6", "5"))
+      text(x=c(.335, .6), y=.00, labels=c("4", "4"))
+    dev.off()
+
 
     # Weighted average
     apply(temp, 2, function(x) (5*x[1] + 4*x[2] + 3*x[3] + 2*x[4] + x[5]) / sum(x) )
@@ -146,13 +198,16 @@ options(digits=2)
 
     temp <- temp[ c(4,1,2,3,5), c(2,4,3,1,5) ]
 
-    par(mar=c(1,1,1,1))
-    mosaicplot(t(temp), main="", las=1, col=paste0("grey", 5:10*10)) 
-    text(x=c(.26, .42, .585, .75, .915), y=c(.875, .84, .775, .75, .72), labels=temp[1,])
-    text(x=c(.26, .42, .585, .74, .915), y=c(.65, .475, .4, .35, .32), labels=temp[2,])
-    text(x=c(.26, .42, .585, .74, .915), y=c(.32, .2, .18, .12, .1), labels=temp[3,])
-    text(x=c(.26, .42), y=c(.07, .05), labels=temp[4,1:2])
-    text(x=c(.585), y=0, labels=temp[5,3])
+    pdf("driver_behaviors_that_need_research.pdf")
+      par(mar=c(1,1,1,1))
+      mosaicplot(t(temp), main="", las=1, col=paste0("grey", 5:10*10)) 
+      text(x=c(.26, .42, .585, .75, .915), y=c(.875, .84, .775, .75, .72), labels=temp[1,])
+      text(x=c(.26, .42, .585, .74, .915), y=c(.65, .475, .4, .35, .32), labels=temp[2,])
+      text(x=c(.26, .42, .585, .74, .915), y=c(.32, .2, .18, .12, .1), labels=temp[3,])
+      text(x=c(.26, .42), y=c(.07, .05), labels=temp[4,1:2])
+      text(x=c(.585), y=0, labels=temp[5,3])
+    dev.off()
+
     
     # Weighted average
     apply(temp, 2, function(x) (5*x[1] + 4*x[2] + 3*x[3] + 2*x[4] + x[5]) / sum(x) )
@@ -165,13 +220,15 @@ options(digits=2)
       colnames(temp) <- c("Women", "Children")
     temp <- t( temp[c(4,1,3,2,5),] )
 
-    par(mar=c(1,1,1,1))
-    mosaicplot(temp, las=1, col=paste0("grey", 5:10*10), main="")
-    text(x=c(.425,.8), y=c(.9,.85), labels=temp[,1])
-    text(x=c(.425,.8), y=c(.68,.525), labels=temp[,2])
-    text(x=c(.425,.8), y=c(.4,.225), labels=temp[,3])
-    text(x=c(.425), y=c(.165), labels=temp[1,4])
-    text(x=c(.425,.8), y=c(0,0), labels=temp[,5])
+    pdf("women_kids_crashes.pdf")
+      par(mar=c(1,1,1,1))
+      mosaicplot(temp, las=1, col=paste0("grey", 5:10*10), main="")
+      text(x=c(.425,.8), y=c(.9,.85), labels=temp[,1])
+      text(x=c(.425,.8), y=c(.68,.525), labels=temp[,2])
+      text(x=c(.425,.8), y=c(.4,.225), labels=temp[,3])
+      text(x=c(.425), y=c(.165), labels=temp[1,4])
+      text(x=c(.425,.8), y=c(0,0), labels=temp[,5])
+    dev.off()
 
     # Weighted average
     apply(temp, 1, function(x) (5*x[1] + 4*x[2] + 3*x[3] + 2*x[4] + x[5]) / sum(x) )
@@ -182,9 +239,10 @@ options(digits=2)
     temp <- table(data$most.pressing.accessibility)
       names(temp) <- c("Commercial\nactivities", "Educational\nopportunities", "Healthcare")
       temp <- temp[c(3,1,2)]
-    par(mar=c(6,4,1,1))
-    barplot(temp, las=2, ylab="# of respondents", ylim=c(0,40))
-
+    pdf("accessibility_issues.pdf")
+      par(mar=c(6,4,1,1))
+      barplot(temp, las=2, ylab="# of respondents", ylim=c(0,40))
+    dev.off()
 
 
 
@@ -200,13 +258,15 @@ options(digits=2)
   
     temp <- temp[ c(2,1,4,5,3), ]
 
-    par(mar=c(1,1,1,1))
-    mosaicplot(temp, las=1, col=paste0("grey", 5:10*10), main="")
-    text(x=c(.215,.73,.91), y=c(.95,.85,.6), labels=c(2,11,23))
-    text(x=c(.215,.38,.55,.73,.91), y=c(.85,.84,.775,.55,.2), labels=temp[,2])
-    text(x=c(.215,.38,.55,.73,.91), y=c(.6,.62,.4,.28,.07), labels=temp[,3])
-    text(x=c(.38,.55,.73), y=c(.35,.175,.1), labels=c("15","8","4"))
-    text(x=c(.215,.38,.73), y=c(.2,.025,0), labels=c(14,4,2))
+    pdf("healthcare_accessibility.pdf")
+      par(mar=c(1,1,1,1))
+      mosaicplot(temp, las=1, col=paste0("grey", 5:10*10), main="")
+      text(x=c(.215,.73,.91), y=c(.95,.85,.6), labels=c(2,11,23))
+      text(x=c(.215,.38,.55,.73,.91), y=c(.85,.84,.775,.55,.2), labels=temp[,2])
+      text(x=c(.215,.38,.55,.73,.91), y=c(.6,.62,.4,.28,.07), labels=temp[,3])
+      text(x=c(.38,.55,.73), y=c(.35,.175,.1), labels=c("15","8","4"))
+      text(x=c(.215,.38,.73), y=c(.2,.025,0), labels=c(14,4,2))
+    dev.off()
 
     # Weighted average
     apply(temp, 1, function(x) (5*x[1] + 4*x[2] + 3*x[3] + 2*x[4] + x[5]) / sum(x) )
@@ -224,18 +284,20 @@ options(digits=2)
       colnames(temp) <- c("Safety education", "Pedestrian facilities", "Transport services")
     temp <- temp[, c(3,1,2) ]
 
-    par(mar=c(1,1,1,1))
-    mosaicplot(t(temp), las=1, col=paste0("grey", 5:10*10), main="")
-    text(x=c(.3,.575,.85), y=c(.75,.85,.9), labels=temp[1,])
-    text(x=c(.3,.575,.85), y=c(.375,.45,.55), labels=temp[2,])
-    text(x=c(.3,.575,.85), y=c(.06,.1,.1), labels=c(temp[4,1],temp[3,2:3]))
+    pdf("education_accessibility.pdf")
+      par(mar=c(1,1,1,1))
+      mosaicplot(t(temp), las=1, col=paste0("grey", 5:10*10), main="")
+      text(x=c(.3,.575,.85), y=c(.75,.85,.9), labels=temp[1,])
+      text(x=c(.3,.575,.85), y=c(.375,.45,.55), labels=temp[2,])
+      text(x=c(.3,.575,.85), y=c(.06,.1,.1), labels=c(temp[4,1],temp[3,2:3]))
+    dev.off()
     
     # Weighted average
     apply(temp, 2, function(x) (5*x[1] + 4*x[2] + 3*x[3] + 2*x[4]) / sum(x) )
 
 
 
-    # Education accessibility
+    # Access
     temp <- matrix(c(5,7,3,0, 6,8,1,0, 1,5,8,1), nrow=4)
     rownames(temp) <- c("Strong relationship",
                         "Direct relationship",
@@ -244,12 +306,14 @@ options(digits=2)
     colnames(temp) <- c("Goods to market", "Labor to market", "Access natural resources")
     temp <- temp[, c(2,1,3) ]
 
-    par(mar=c(1,1,1,1))
-    mosaicplot(t(temp), las=1, col=paste0("grey", 5:9*10), main="")
-    text(x=c(.28,.575,.85), y=c(.78,.825,.95), labels=temp[1,])
-    text(x=c(.28,.575,.85), y=c(.35,.45,.75), labels=temp[2,])
-    text(x=c(.28,.575,.85), y=c(.01,.075,.325), labels=c(temp[3,]))
-    text(.85, 0, "1")
+    pdf("access.pdf")
+      par(mar=c(1,1,1,1))
+      mosaicplot(t(temp), las=1, col=paste0("grey", 5:9*10), main="")
+      text(x=c(.28,.575,.85), y=c(.78,.825,.95), labels=temp[1,])
+      text(x=c(.28,.575,.85), y=c(.35,.45,.75), labels=temp[2,])
+      text(x=c(.28,.575,.85), y=c(.01,.075,.325), labels=c(temp[3,]))
+      text(.85, 0, "1")
+    dev.off()
 
     # Weighted average
     apply(temp, 2, function(x) (5*x[1] + 4*x[2] + 3*x[3] + 2*x[4]) / sum(x) )
@@ -257,9 +321,11 @@ options(digits=2)
 
 
     # Road dust
-    par(mar=c(2.5,4,1,1))      
-    barplot(table(data$inadequate.attention.dust), 
-            ylab="frequency")
+    pdf("road_dust.pdf")
+      par(mar=c(2.5,4,1,1))      
+      barplot(table(data$inadequate.attention.dust), 
+              ylab="frequency")
+    dev.off()
     table(data$inadequate.attention.dust) / sum(table(data$inadequate.attention.dust))
 
 
@@ -271,51 +337,46 @@ options(digits=2)
       colnames(temp) <- c("Most vulnerable", "Vulnerable", "Least vulnerable")
     temp <- temp[ c(1,3,2), ]
 
-    par(mar=c(1,1,1,1))
-    mosaicplot(temp, las=1, col=c("grey50", "grey70", "grey90"), main="")
-    text(x=c(.28,.57,.85), y=c(.72,.78,.91), labels=temp[,1])
-    text(x=c(.28,.57,.85), y=c(.28,.32,.73), labels=temp[,2])
-    text(x=c(.28,.57,.85), y=c(.01,.0,.31), labels=temp[,3])
+    pdf("air_pollution.pdf")
+      par(mar=c(1,1,1,1))
+      mosaicplot(temp, las=1, col=c("grey50", "grey70", "grey90"), main="")
+      text(x=c(.28,.57,.85), y=c(.72,.78,.91), labels=temp[,1])
+      text(x=c(.28,.57,.85), y=c(.28,.32,.73), labels=temp[,2])
+      text(x=c(.28,.57,.85), y=c(.01,.0,.31), labels=temp[,3])
+    dev.off()
 
     # Weighted average
     apply(temp, 1, function(x) (5*x[1] + 3*x[2] + x[3]) / sum(x) )
 
 
 
-    # Air pollution
-    temp <- cbind( table(data$PCS.building.roads),
-                   table(data$PCS.transport.provision) )
-      colnames(temp) <- c("Build roads", "Transport provision")
-    temp <- temp[ c(5,1,2,4,3), ]
-
-    par(mar=c(1,1,1,1))
-    mosaicplot(t(temp), las=1, col=paste0("grey", 5:10*10), main="")
-    text(x=c(.39,.8), y=c(.78,.74), labels=temp[1,])
-    text(x=c(.39,.8), y=c(.42, .33), labels=temp[2,])
-    text(x=c(.39,.8), y=c(.16,.1), labels=temp[3,])
-
-    # Weighted average
-    apply(temp, 1, function(x) (5*x[1] + 3*x[2] + x[3]) / sum(x) )
-
-
-
-# PCS
-table(data$PCS.building.roads, data$PCS.transport.provision)
-pcs.rules <- apriori(subset(data, select=grep('PCS', names(data))), parameter = list(supp = 0.05, conf = 0.8))
-inspect( sort(pcs.rules, by='lift', decreasing=T) )
+#     # PCS
+#     temp <- cbind( table(data$PCS.building.roads),
+#                    table(data$PCS.transport.provision) )
+#       colnames(temp) <- c("Build roads", "Transport provision")
+#     temp <- temp[ c(5,1,2,4,3), ]
+# 
+#     par(mar=c(1,1,1,1))
+#     mosaicplot(t(temp), las=1, col=paste0("grey", 5:10*10), main="")
+#     text(x=c(.39,.8), y=c(.78,.74), labels=temp[1,])
+#     text(x=c(.39,.8), y=c(.42, .33), labels=temp[2,])
+#     text(x=c(.39,.8), y=c(.16,.1), labels=temp[3,])
+# 
+#     # Weighted average
+#     apply(temp, 1, function(x) (5*x[1] + 3*x[2] + x[3]) / sum(x) )
 
 
 
-  # Survey collection dates
-  summary( as.Date(data$StartDate) )
+# # PCS
+# table(data$PCS.building.roads, data$PCS.transport.provision)
+# pcs.rules <- apriori(subset(data, select=grep('PCS', names(data))), parameter = list(supp = 0.05, conf = 0.8))
+# inspect( sort(pcs.rules, by='lift', decreasing=T) )
 
 
-  # Time to take the survey (minutes)
-  summary( as.numeric(data$survey_length_of_time) )
 
 
-  # Good participation (% that agreed to answer a few questions)
-  100 * round( mean( as.character(data$Interested_in_answering_one_last_question_) == 'Yes', na.rm = TRUE), 2)
+
+
 
 
 
@@ -402,111 +463,3 @@ itemFrequencyPlot(data)
 View(data)  
 
 
-# Distribution of countries respondents have worked in?  How does this compare to the distribution of international transportation experts?  
-# Countries worked in versus priorities?
-# Background versus countries worked in?
-# Background versus priorities?
-
-
-
-
-
-# No demographic Qs?
-# 
-
-
-
-
-# Survey continent and country
-URL <- paste('http://www.iplocation.net/index.php?query=',
-             data$IP.Address,
-             sep='')
-webpages <- getURL(url = URL)
-
-data$continent <- sapply(X=webpages, FUN=function(x) as.character(readHTMLTable(x)[[13]][7,2]))
-data$country <- sapply(X=webpages, FUN=function(x) as.character(readHTMLTable(x)[[12]][3,2]))
-
-round(x=table(data$continent) / nrow(data), 
-      digits=2)
-
-round(x=table(data$country) / nrow(data), 
-      digits=2)
-
-
-
-
-# Calculate time that respondent spent on the survey
-data$StartDate <- as.POSIXct(x = strptime(x = data$StartDate,
-                                          format = "%m/%d/%Y %T"))
-data$EndDate <- as.POSIXct(x = strptime(x = data$EndDate, 
-                                        format = "%m/%d/%Y %T"))
-data$survey.length.of.time <- (data$EndDate - data$StartDate)
-
-
-
-
-# Drop a few variables that are useless for our analysis
-data <- subset(x = data, 
-               select = -c(RespondentID,
-                           CollectorID,
-                           StartDate,
-                           EndDate,
-                           Email.Address,
-                           First.Name,
-                           LastName))
-
-
-
-
-
-# k-fold cross-validation for 1 to m clusters
-k=5
-m=5
-results <- matrix(NA, nrow=k, ncol=m)
-dfchunk <- split(x=temp, factor( sample(1:nrow(temp), nrow(temp), replace=F) %% k) )
-
-for(i in 1:k){
-  
-  i=1
-  
-  temp_data <- dfchunk
-  test <- temp_data[[i]]
-  
-  temp_data[[i]] <- NULL
-  train <- do.call(what=rbind, args=temp_data)
-  
-  for(j in 1:m){
-    m=7
-    fold_clustering <- kmeansvar(X.quali=train, init=m)
-    fold_clustering
-    fold_clustering$cluster
-    
-    fold_2 <- hclustvar(X.quali=train)
-    stability(fold_2)
-    
-    names(train)
-    names(test)
-    
-    summary(fold_clustering)
-  }
-  
-}
-
-
-
-t <- kmeansvar(X.quali=temp, init=3)
-predict(t)
-summary(t)
-t$var
-t$scores
-
-
-
-# Distribution of countries respondents have worked in?
-# Countries worked in versus priorities?
-# Background versus countries worked in?
-# Background versus priorities?
-
-
-# No demographic Qs?
-# 
